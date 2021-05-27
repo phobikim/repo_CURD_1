3f13d5171200514ec0f9f5b3285028cc22199d78
김민정 <mjkim8038@raoncorp.com>
2021-05-27 16:05:53 +0900

    #실제 비즈니스로직실행과 jsp 연결을 위한 action 인터페이스생성

---
+++ jsptest/src/main/java/action/ListBookAction.java
@@ -0,0 +1,19 @@
+package action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ListBookAction implements CRUDAction{
+
+	@Override
+	public String whichJsp(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		
+		return null;
+	}
+
+}
