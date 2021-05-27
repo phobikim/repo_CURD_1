b9f04a7fdbae538b673422b88d08c0ee4764fe16
김민정 <mjkim8038@raoncorp.com>
2021-05-27 11:15:45 +0900

    #CRUDAction 인터페이스 whichJsp 함수 생성

--- jsptest/src/main/java/action/CRUDAction.java
+++ jsptest/src/main/java/action/CRUDAction.java
@@ -1,5 +1,13 @@
 package action;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//해당 Action이 수행 한 후에 어떤 jsp파일로 가야할지 파일 이름을 반환
 public interface CRUDAction {
 
+	public String whichJsp(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 }
